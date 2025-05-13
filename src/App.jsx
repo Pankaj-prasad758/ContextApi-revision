8472dd843c49bf7a2093a0f93d4dcd64837ad7bd
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-05-13 16:22:53 +0530

    project started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-black p-4 rounded-2xl text-green-400 w-full">
+        Context API{" "}
+      </h1>
+    </>
+  );
+}
+
+export default App;
